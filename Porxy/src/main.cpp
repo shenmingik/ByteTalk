a1b357e36b468ee0eb2e800db3b8ce1d66733844
ik-ubuntu <2107810343@qq.com>
2021-05-04 22:26:43 +0800

    write over

---
+++ Porxy/src/main.cpp
@@ -0,0 +1,33 @@
+#include <signal.h>
+
+#include <rpc/RpcApplication.hpp>
+#include <rpc/RpcConfigure.hpp>
+
+#include "ProxyService.hpp"
+using namespace std;
+
+//处理服务器ctrl+c结束后，重置user的状态信息
+void reset_handler(int)
+{
+    ProxyService::get_instance().reset();
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    RpcApplication::init(argc, argv);
+    RpcConfigure configure = RpcApplication::get_instance().get_configure();
+    string ip = configure.find_load("server_ip");
+    int port = atoi(configure.find_load("server_port").c_str());
+
+    signal(SIGINT, reset_handler);
+
+    muduo::net::EventLoop loop;
+    muduo::net::InetAddress addr(ip, port);
+    ProxyServer server(loop, addr, "ProxyServer");
+
+    server.start();
+    loop.loop();
+
+    return 0;
+}
